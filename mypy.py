f2a1de19ccc6440a125d29430329e5c2c72fab71
Arvind Singh <122955926+rawatarvind@users.noreply.github.com>
2024-05-07 21:27:19 +0530

    Create mypy.py

---
+++ mypy.py
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+list1 = [ "amit", "pankaj", 45849, "suraj","pankaj"]
+
+x = "awesome"
+
+def myfunc():
+    x = "fantastic"
+    print("pyton is ", x)
+
+myfunc()
+
+
+print("python is", x)
