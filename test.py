5054b434c1da42dd7296718a5591fc8efbd003de
Arvind Singh <122955926+rawatarvind@users.noreply.github.com>
2024-05-07 21:18:29 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if 5>2 :
+    print ("five is greater than two")
+
+else:
+    print ("there is error")
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+
+a="hello, world!"
+print(a.split(","))
+
+age=36
+txt="my name is john, I am", age
+print(txt)
