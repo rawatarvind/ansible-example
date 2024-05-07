00a59348936a4c979c83893bceaa5d6dccbbf161
Arvind Singh <122955926+rawatarvind@users.noreply.github.com>
2024-05-07 21:22:40 +0530

    Create thread.py

---
+++ thread.py
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+import time
+def move():
+    for i in range(5):
+        print("object moving")
+        time.sleep(1)
+
+def jump():
+    for i in range(5):
+        print("object jumping")
+        time.sleep(1)
