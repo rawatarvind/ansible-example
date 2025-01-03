92562ae33e1af45e6b00649424e2dbe8a4c9496d
rawatarvind <iamashurawat@gmail.com>
2025-01-03 20:05:54 +0530

    apache webserver yaml configure

--- script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-hostname
-echo "OS:`cat /etc/redhat-release`"
-echo "Kernel: `uname -r`"
-echo "Uptime: `uptime -p`"
-echo "Packages: `dpkg --list | wc -l`"
--- test.sh
+++ test.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-arg=$1
-echo $arg
+# Define colors
+RED='\033[0;31m'
+RESET='\033[0m'
+
+# Get hostname
+HOSTNAME=$(hostname)
+
+# Print "hostname" in block style
+echo -e "${RED}"
+echo "###############################################"
+for (( i=0; i<${#HOSTNAME}; i++ )); do
+  CHAR=${HOSTNAME:$i:1}
+  case "$CHAR" in
+    [aA]) 
+      echo "#    ##   ##   ##    #" 
+      echo "#   #  #  # #  #  #  #"
+      echo "#   ####  # #  ####  #"
+      ;;
+    🎯%convert#"""
